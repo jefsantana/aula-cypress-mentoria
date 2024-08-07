7b74877f769409db27946b8f861951a5943aa334
jefsantana <jefersonsm.js@gmail.com>
2024-08-07 15:41:14 -0300

    url add

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,4 +1,4 @@
-import userData from '../fixtures/users/userData.json'
+import userData from '../fixtures/userData.json';
 
 describe('Orange HRM Tests', () => {
 
@@ -9,24 +9,24 @@ describe('Orange HRM Tests', () => {
     selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']"
-  }
+  };
 
   it('login - sucesso', () => {    
-    cy.visit('/auth/login') 
-    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname', { timeout: 10000 }).should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.dashboardGrid, { timeout: 10000 }).should('exist')
-    cy.get(selectorList.selectionTitleTopBar).contains('Dashboard')
-  })
+    cy.visit('/auth/login'); 
+    cy.get(selectorList.usernameField).type(userData.userSuccess.username);
+    cy.get(selectorList.passwordField).type(userData.userSuccess.password);
+    cy.get(selectorList.loginButton).click();
+    cy.location('pathname', { timeout: 10000 }).should('equal', '/web/index.php/dashboard/index');
+    cy.get(selectorList.dashboardGrid, { timeout: 10000 }).should('exist');
+    cy.get(selectorList.selectionTitleTopBar).contains('Dashboard');
+  });
 
-  it('login - fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert, { timeout: 10000 }).should('be.visible')
-    cy.location('pathname', { timeout: 10000 }).should('equal', '/web/index.php/auth/login')
-  })
-})
+  it('login - falha', () => {
+    cy.visit('/auth/login');
+    cy.get(selectorList.usernameField).type(userData.userFail.username);
+    cy.get(selectorList.passwordField).type(userData.userFail.password);
+    cy.get(selectorList.loginButton).click();
+    cy.get(selectorList.wrongCredentialAlert, { timeout: 10000 }).should('be.visible');
+    cy.location('pathname', { timeout: 10000 }).should('equal', '/web/index.php/auth/login');
+  });
+});
